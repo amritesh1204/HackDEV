97fe5ee4a6c22b558dbe84919819cbc27ea8c447
Nickitesh_Singh <amriteshsingh2021@outlook.com>
2024-04-08 23:06:45 +0530

    added homepage css

--- frontend/loginRegister/src/pages/homepage.jsx
+++ frontend/loginRegister/src/pages/homepage.jsx
@@ -1,6 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Chatbot from 'react-chatbot-kit'
 import 'react-chatbot-kit/build/main.css'
+// eslint-disable-next-line no-unused-vars
 import Navbar from './navbar'
 import Layout from './layout'
 import '../styles/homepage.css'
